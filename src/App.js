812080ccd35719a97fb417eeba02a10088724120
Mounir-Bmz <mounir.boumaza.dev@gmail.com>
2025-04-18 17:03:36 +0200

    Add and style Cart component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import Banner from './components/Banner';
+import Cart from './components/Cart';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Banner />
+        <Cart />
     </div>
   );
 }
 
 export default App;
-// Test change
